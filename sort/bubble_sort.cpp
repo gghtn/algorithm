b2dc4048ec46cdb581fad97460b834ce0926af84
gghtn <ghtn9898@gmail.com>
2022-09-27 22:54:02 +0900

    bubble sort

--- sort/bubble_sort.cpp
+++ sort/bubble_sort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void bubble_sort(int arr[], int arrSize) {
-	for (int i = 0; i < arrSize - 1; i++) {
-		for (int j = 1; j < arrSize - i; j++) {
-			if (arr[j] < arr[j - 1]) swap(arr[j], arr[j - 1]);
+	for (int i = arrSize - 1; i >= 0; i--) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j + 1] < arr[j]) swap(arr[j], arr[j + 1]);
 		}
 	}
 }
